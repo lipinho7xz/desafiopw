a4858f884f568d66ca42ea8731b530f9177091b9
lipinho7xz <lipezzs18@gmail.com>
2025-05-05 20:10:53 -0300

    Add files via upload

--- desafiopw/desafiopw3.php
+++ desafiopw/desafiopw3.php
@@ -1,1 +1,24 @@
-md
+<?php
+$us1 = "Chico";
+$alt1 = 1.50;
+$us2 = "Juca";
+$alt2 = 1.10;
+$cont = 0;
+
+while ($alt2 < $alt1){
+
+$alt1 += 0.02;
+$alt2 += 0.03;
+$cont ++;
+
+}
+ 
+echo "Será Necessario $cont anos para juca passar chico";
+ 
+ 
+
+ 
+ 
+ 
+?>
+ 
